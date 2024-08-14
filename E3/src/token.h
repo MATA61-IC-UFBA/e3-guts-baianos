759c70ed5f036c5282d8dc6d5c38879e0adc8edc
Inovar para Pessoas Negras <silvaarthur@ufba.br>
2024-08-14 10:09:29 -0300

    atualizando token.h

--- E3/src/token.h
+++ E3/src/token.h
@@ -1,12 +1,32 @@
 /* token.h */
-
 typedef enum {
         ID = 256, 
 	NUM, 
-        KEY,
-        SYM,  
-        SYMC, 
-        CHAR,
+        // KEY, 
+        ARRAY,          //KEYWORD
+        BOOLEAN,        //KEYWORD
+        CHAR,           //KEYWORD
+        ELSE,           //KEYWORD
+        FALSE,          //KEYWORD
+        FOR,            //KEYWORD
+        FUNCTION,       //KEYWORD
+        IF,             //KEYWORD
+        INTEGER,        //KEYWORD
+        PRINT,          //KEYWORD
+        RETURN,         //KEYWORD
+        STRING,         //KEYWORD
+        TRUE,           //KEYWORD
+        VOID,           //KEYWORD
+        WHILE,          //KEYWORD
+        // SYM,         AGORA RETORNA VALOR ASC   
+        // SYMC,
+        LTE,            // SYMC (<=)
+        GTE,            // SYMC (>=)
+        EQ,             // SYMC (==)
+        NEQ,            // SYMC (!=)
+        AND,            // SYMC (&&)
+        OR,             // SYMC (||)
+        // CHAR,        AGORA RETORNA VALOR ASC
 	STR,
 	ERROR
 } token_t; 
